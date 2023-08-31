2bc2897333ac040e7885c3418d071ec3216f444c
Le_Defender <padreshelby@gmail.com>
2023-08-31 03:45:41 +0300

    register login page done

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,34 @@
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main-blue.svg";
+import { Link } from "react-router-dom";
+import { Logo } from "../components";
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            I&apos;m baby master cleanse locavore gochujang, air plant neutral
+            milk hotel salvia whatever tbh same mlkshk sus food truck keffiyeh.
+            Ascot godard man bun tumblr shaman mumblecore intelligentsia.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn ">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
 };
+
 export default Landing;
